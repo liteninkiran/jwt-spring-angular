3cc7a2f5460a4f937c8bb7c23838a5cbcb35c3da
Kiran Anand <liteninkiran@hotmail.com>
2022-08-21 16:21:31 +0100

    Authentication service

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [
@@ -11,6 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
     imports: [
         BrowserModule,
         AppRoutingModule,
+        HttpClientModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
--- front-end/src/app/service/authentication.service.ts
+++ front-end/src/app/service/authentication.service.ts
@@ -1,9 +1,67 @@
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
 
-@Injectable({
-    providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class AuthenticationService {
+    public host = environment.apiUrl;
+    private token: string | null = null;
+    private loggedInUsername: string | null = null;
+    private jwtHelper = new JwtHelperService();
 
-    constructor() { }
+    constructor(private http: HttpClient) { }
+
+    public login(user: User): Observable<HttpResponse<User>> {
+        return this.http.post<User>(`${this.host}/user/login`, user, { observe: 'response' });
+    }
+
+    public register(user: User): Observable<User> {
+        return this.http.post<User>(`${this.host}/user/register`, user);
+    }
+
+    public logOut(): void {
+        this.token = null;
+        this.loggedInUsername = null;
+        localStorage.removeItem('user');
+        localStorage.removeItem('token');
+        localStorage.removeItem('users');
+    }
+
+    public saveToken(token: string): void {
+        this.token = token;
+        localStorage.setItem('token', token);
+    }
+
+    public addUserToLocalCache(user: User): void {
+        localStorage.setItem('user', JSON.stringify(user));
+    }
+
+    public getUserFromLocalCache(): User {
+        return JSON.parse(localStorage.getItem('user') ?? '');
+    }
+
+    public loadToken(): void {
+        this.token = localStorage.getItem('token');
+      }
+    
+    public getToken(): string | null {
+        return this.token;
+    }
+    
+    public isUserLoggedIn(): boolean {
+        this.loadToken();
+        if (this.token != null && this.token !== '') {
+            if (this.jwtHelper.decodeToken(this.token).sub != null || '') {
+                if (!this.jwtHelper.isTokenExpired(this.token)) {
+                    this.loggedInUsername = this.jwtHelper.decodeToken(this.token).sub;
+                    return true;
+                }
+            }
+        }
+        this.logOut();
+        return false;
+    }
 }
