59a06961caf2c77b7ed63f5725843861838ad3a8
Kiran Anand <liteninkiran@hotmail.com>
2022-08-21 11:26:15 +0100

    Email config

--- src/main/java/com/supportportal/constant/EmailConstant.java
+++ src/main/java/com/supportportal/constant/EmailConstant.java
@@ -2,12 +2,12 @@ package com.supportportal.constant;
 
 public class EmailConstant {
     public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
-    public static final String USERNAME = "infojuniorro@gmail.com";
-    public static final String PASSWORD = "N99gmail1@";
-    public static final String FROM_EMAIL = "support@getarrays.com";
+    public static final String USERNAME = "";  // Enter email address
+    public static final String PASSWORD = ""; // Enter password
+    public static final String FROM_EMAIL = "";
     public static final String CC_EMAIL = "";
-    public static final String EMAIL_SUBJECT = "Get Arrays, LLC - New Password";
-    public static final String GMAIL_SMTP_SERVER = "smtp.gmail.com";
+    public static final String EMAIL_SUBJECT = "Registration Complete";
+    public static final String MAIL_SMTP_SERVER = "smtp.gmail.com";
     public static final String SMTP_HOST = "mail.smtp.host";
     public static final String SMTP_AUTH = "mail.smtp.auth";
     public static final String SMTP_PORT = "mail.smtp.port";
